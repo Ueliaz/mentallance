9de717da86a8e28393900ee71401249811661067
Unelias <fatihhafif27@gmail.com>
2023-07-05 10:05:22 +0300

    Doktor giris ve kayit sayfalari olusturuldu

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,7 @@
+library app_theme;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+part 'utils/colors.dart';
+
+
---
+++ lib/view/doctor_authentication/doctor_singup.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mentallance/components/reusable_widgets/reusable_button.dart';
+import 'package:mentallance/theme/app_theme.dart';
+
+import '../../components/reusable_widgets/reusable_text_field.dart';
+
+class Doctor_singUp extends StatefulWidget {
+  const Doctor_singUp({Key? key}) : super(key: key);
+
+  @override
+  _Doctor_singUpState createState() => _Doctor_singUpState();
+}
+
+class _Doctor_singUpState extends State<Doctor_singUp> {
+  TextEditingController _userNameTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: customAppBarr(context, 'Hesap Oluştur'),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 120, 20, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            reusableTextField("Kullanıcı Adınızı giriniz", Icons.person_outline,
+                false, _userNameTextController),
+            const SizedBox(
+              height: 20,
+            ),
+            reusableTextField("E-mail Adresinizi giriniz", Icons.person_outline,
+                false, _emailTextController),
+            const SizedBox(
+              height: 20,
+            ),
+            reusableTextField("Şifrenizi giriniz", Icons.lock_outlined, true,
+                _passwordTextController),
+            const SizedBox(
+              height: 20,
+            ),
+            reusableButton(context, "Sign Up", colorCollection[1], () {
+              //TODO ; 
+              //* hesap olusturma icin veri tabanina baglanacak
+              //* hata kontrolu yapilacak
+
+
+
+            })
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+@override
+AppBar customAppBarr(BuildContext context, String title) {
+  return AppBar(title: Text(title));
+}
