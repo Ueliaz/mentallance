3e68884b05939717b5a2f362ef282d92db15ea2e
Nefise Şevval Açıkalın <sevvalacikalin@gmail.com>
2023-07-12 12:03:58 +0300

    m

--- lib/view/Doctor_interface/client_list_page/customer_list.dart
+++ lib/view/Doctor_interface/client_list_page/customer_list.dart
@@ -1,65 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+library customer;
 
-class CustomerList extends StatelessWidget {
-  const CustomerList({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+
+import '../../../components/reusable_widgets/reusable_button.dart';
+import '../../../theme/app_theme.dart';
+import 'add_customer.dart';
+
+
+class CustomerList extends StatefulWidget {
+  const CustomerList({super.key});
 
   @override
+  State<CustomerList> createState() => _CustomerListState();
+}
+
+class _CustomerListState extends State<CustomerList> {
+  @override
   Widget build(BuildContext context) {
-    final currentUser = FirebaseAuth.instance.currentUser;
-
-    if (currentUser == null) {
-      // Kullanıcı oturum açmamışsa, gerekli işlemleri yapabilirsiniz.
-      // Örneğin, giriş sayfasına yönlendirme yapabilirsiniz.
-      return Scaffold(
-        body: Center(
-          child: Text('Kullanıcı oturumu açmamış'),
-        ),
-      );
-    }
-
     return Scaffold(
       body: SafeArea(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('KayitOlanDoktor')
-              .where('DoktorId', isEqualTo: currentUser.uid)
-              .snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Text('Hata: ${snapshot.error}');
-            }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
-
-            final patientList = snapshot.data!.docs;
-
-            if (patientList.isEmpty) {
-              // Hasta listesi boş ise, gerekli işlemleri yapabilirsiniz.
-              // Örneğin, boş liste mesajı görüntüleyebilirsiniz.
-              return Scaffold(
-                body: Center(
-                  child: Text('Hasta bulunamadı'),
-                ),
-              );
-            }
-
-            return ListView.builder(
-              itemCount: patientList.length,
-              itemBuilder: (context, index) {
-                final patientData = patientList[index].data() as Map<String, dynamic>;
-                final email = patientData['DanisanEmail'] as String?;
-
-                return ListTile(
-                  title: Text(email ?? 'No email found'),
-                  subtitle: Text('$email adresine sahip danışan'),
-                );
-              },
-            );
-          },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: reusableButton(
+                    context, 'YENİ DANISAN Ekle', colorCollection[1], () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>const AddClient() ,) );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
