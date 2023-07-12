88f3ab95560cf4fd61fe313562fa0bd654cb8dd3
Nefise Şevval Açıkalın <sevvalacikalin@gmail.com>
2023-07-12 16:06:06 +0300

    Randevular koleksiyonunda doktorıd geliyor

--- lib/view/Appointment/appointment_page.dart
+++ lib/view/Appointment/appointment_page.dart
@@ -12,6 +12,67 @@ class _AppointmentPageState extends State<AppointmentPage> {
   String doctorId = '';
   String selectedTime = '';
 
+  Future<void> addAppointment(
+      String selectedDoctorId, String userId, String selectedTime) async {
+    // Kullanıcının doktorunu almak için veritabanından sorgu yapın
+    final userSnapshot = await FirebaseFirestore.instance
+        .collection('KayitOlanDanisan')
+        .doc(userId)
+        .get();
+
+    // Kullanıcının doktorId değerini alın
+    final userDoctorId = userSnapshot['DoktorId'];
+
+    // Kullanıcının doktoru yoksa hata mesajı gösterin
+    if (userDoctorId == null || userDoctorId.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Hata'),
+            content: const Text('Kullanıcının atanmış bir doktoru yok.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Tamam'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    // Seçilen doktorId değerini Randevular koleksiyonuna ekle
+    await FirebaseFirestore.instance.collection('Randevular').add({
+      'DoktorId': userDoctorId,
+      'DanisanId': userId,
+      'RandevuTarihi': today,
+      'RandevuSaati': selectedTime,
+    });
+
+    // Randevu başarıyla eklendiyse kullanıcıya mesaj gösterin
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Başarılı'),
+          content: const Text('Randevu başarıyla eklendi.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Tamam'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Future<List<String>> getAvailableAppointmentTimes(String doctorId) async {
     final appointmentsSnapshot = await FirebaseFirestore.instance
         .collection('KayitOlanDanisan')
@@ -78,15 +139,10 @@ class _AppointmentPageState extends State<AppointmentPage> {
                         TextButton(
                           onPressed: () async {
                             if (availableTimes.contains(selectedTime)) {
-                              // Seçilen randevu saati, müsait saatler arasında yer alıyor
-
-  // Veritabanına randevu kaydını eklemek için gerekli işlemleri gerçekleştirin
-  await FirebaseFirestore.instance.collection('Randevular').add({
-    'DoktorId': doctorId,
-    'DanisanId': FirebaseAuth.instance.currentUser!.uid,
-    'RandevuTarihi': today,
-    'RandevuSaati': selectedTime,
-  });
+                              await addAppointment(
+                                  doctorId,
+                                  FirebaseAuth.instance.currentUser!.uid,
+                                  selectedTime);
                               Navigator.of(context).pop();
                             } else {
                               // Kullanıcı bir saat seçmediğinde hata mesajı gösterin
@@ -95,7 +151,8 @@ class _AppointmentPageState extends State<AppointmentPage> {
                                 builder: (BuildContext context) {
                                   return AlertDialog(
                                     title: const Text('Hata'),
-                                    content: const Text('Lütfen bir saat seçin.'),
+                                    content:
+                                        const Text('Lütfen bir saat seçin.'),
                                     actions: [
                                       TextButton(
                                         onPressed: () {
