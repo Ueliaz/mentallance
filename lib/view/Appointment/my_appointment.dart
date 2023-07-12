8af7bda5e177188ad5f2fbd6d87965b39ade52cc
gizem-ce <gizemcumen85@gmail.com>
2023-07-12 09:59:53 +0300

    My_appointment page done

---
+++ lib/view/Appointment/my_appointment.dart
@@ -0,0 +1,95 @@
+
+/*
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CalendarScreen(),
+    );
+  }
+}
+*/
+
+
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  List<String> topBarTitles = ['Gelecek Randevular', 'Geçmiş Randevular', 'Silinen Randevular'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Takvim'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: _focusedDay.subtract(Duration(days: 7)),
+            lastDay: _focusedDay.add(Duration(days: 7)),
+            calendarFormat: _calendarFormat,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            calendarStyle: CalendarStyle(
+              selectedDecoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Colors.grey[300],
+                shape: BoxShape.circle,
+              ),
+              todayTextStyle: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: List.generate(topBarTitles.length, (index) {
+              return Container(
+                width: MediaQuery.of(context).size.width / 3,
+                height: 40,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    topBarTitles[index],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
