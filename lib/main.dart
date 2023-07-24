41bf8ebc9868ed485dbf88d58903cd066d045439
burakhkahraman <1190606041@nku.edu.tr>
2023-07-24 16:48:06 +0300

    Argenova Internship Program Mental Health Mobile Application

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,8 @@ import 'package:mentallance/view/Profile/client_profile.dart';
 import 'package:mentallance/view/Profile/doctor_profile.dart';
 import 'package:mentallance/view/Settings/settings.dart';
 
-
 import 'package:mentallance/view/entry_page/entry_page_view_mobile.dart';
 import 'firebase_options.dart';
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +17,7 @@ void main() async {
   );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -28,7 +26,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
       home: const EntryPageView(),
@@ -50,46 +47,51 @@ class _DocBottonNavBarState extends State<DocBottonNavBar> {
     DoctorProfilePage(),
     MyWidget(),
     SettingsPage(),
-  ];  
-  
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Container(decoration: BoxDecoration(color: lightColorScheme.primaryContainer, borderRadius: BorderRadius.circular(50)),
+        child: Container(
+          decoration: BoxDecoration(
+              color: lightColorScheme.primaryContainer,
+              borderRadius: BorderRadius.circular(50)),
           child: GNav(
             padding: const EdgeInsets.all(8),
             activeColor: lightColorScheme.onPrimaryContainer,
-            tabActiveBorder: Border.all(color: lightColorScheme.onPrimaryContainer, width: 1),
+            tabActiveBorder: Border.all(
+                color: lightColorScheme.onPrimaryContainer, width: 1),
             color: lightColorScheme.onSecondaryContainer,
             gap: 8,
-            
-            tabs:const  [
-            GButton(
-              icon: Icons.person,
-              text: 'Profil',
-            ),
-            GButton(
-              icon: Icons.medical_information,
-              text: 'Hastalarim',
-            ),
-            GButton(
-              icon: Icons.settings,
-              text: 'Ayarlar',
-            ),
-        
-            
-          ],onTabChange: (value) {
-            setState(() {
-              _selectedIndex = value;
-            });
-          },),
+            tabs: const [
+              GButton(
+                icon: Icons.person,
+                text: 'Profil',
+              ),
+              GButton(
+                icon: Icons.medical_information,
+                text: 'Hastalarim',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Ayarlar',
+              ),
+            ],
+            onTabChange: (value) {
+              setState(() {
+                _selectedIndex = value;
+              });
+            },
+          ),
         ),
-      ),   body: _widgetOptions.elementAt(_selectedIndex),   
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
     );
   }
 }
+
 class CusBottonNavBar extends StatefulWidget {
   const CusBottonNavBar({super.key});
 
@@ -107,45 +109,47 @@ class _CusBottonNavBarState extends State<CusBottonNavBar> {
     ClientProfilePage(),
     OdevListe(),
     SettingsPage(),
-    
-    
-  ];  
-  
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Container(decoration: BoxDecoration(color: lightColorScheme.primaryContainer, borderRadius: BorderRadius.circular(50)),
+        child: Container(
+          decoration: BoxDecoration(
+              color: lightColorScheme.primaryContainer,
+              borderRadius: BorderRadius.circular(50)),
           child: GNav(
             padding: const EdgeInsets.all(8),
             activeColor: lightColorScheme.onPrimaryContainer,
-            tabActiveBorder: Border.all(color: lightColorScheme.onPrimaryContainer, width: 1),
+            tabActiveBorder: Border.all(
+                color: lightColorScheme.onPrimaryContainer, width: 1),
             color: lightColorScheme.onSecondaryContainer,
             gap: 8,
-            
-            tabs:const  [
-            GButton(
-              icon: Icons.person,
-              text: 'Profil',
-            ),
-            GButton(
-              icon: Icons.medical_services,
-              text: 'Görevlerim',
-            ),
-            GButton(
-              icon: Icons.settings,
-              text: 'Ayarlar',
-            ),
-        
-            
-          ],onTabChange: (value) {
-            setState(() {
-              _selectedIndex = value;
-            });
-          },),
+            tabs: const [
+              GButton(
+                icon: Icons.person,
+                text: 'Profil',
+              ),
+              GButton(
+                icon: Icons.medical_services,
+                text: 'Görevlerim',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Ayarlar',
+              ),
+            ],
+            onTabChange: (value) {
+              setState(() {
+                _selectedIndex = value;
+              });
+            },
+          ),
         ),
-      ),   body: _widgetOptions.elementAt(_selectedIndex),   
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
     );
   }
 }
